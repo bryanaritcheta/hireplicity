549d56b0cbdae378fd6ea8acd4eec4089ea79005
Bryan Aritcheta <bryanaritcheta@gmail.com>
2022-02-26 14:12:06 +0800

    Cleanup boiler plate react app

---
+++ technicalpartone/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ComputeFactorial from './components/ComputeFactorial'
+
+function App() {
+  return (
+    <div className="App">
+      <ComputeFactorial />
+    </div>
+  );
+}
+
+export default App;
