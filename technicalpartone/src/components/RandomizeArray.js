76c312db964c991e8b7b0d73b1f293a649ad7653
Bryan Aritcheta <bryanaritcheta@gmail.com>
2022-02-26 17:14:06 +0800

    Updates for key handling of array items

--- technicalpartone/src/components/RandomizeArray.js
+++ technicalpartone/src/components/RandomizeArray.js
@@ -17,18 +17,13 @@ const sampleArray = [
 function RandomizeArray() {
     const [, arrayRandom] = useState(sampleArray.sort((a, b) => 0.5 - Math.random()))
 
-    // const arrayRandomize = (arraytorandomize) => {
-    //     arraytorandomize = sampleArray.sort((a, b) => 0.5 - Math.random())
-    //     arrayRandom(arraytorandomize)
-    // }
-
     return (
         <div id="section-is-contained">
             <h2>Randomize an array.</h2>
             <div>
                 <h3>Randomized array: [
                     {sampleArray.map((n) => (
-                        <span className="sampleArrayItem" key={n}>{n}</span>
+                        <span className="sampleArrayItem" key={n + Math.random()}>{n}</span>
                     ))}
                     ]
                 </h3>
